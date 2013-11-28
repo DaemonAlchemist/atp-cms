cc36dee199608dc9beecbe0ec0951b55abe54ae0
Andrew Wittrock <andy@wittrock.us>
2013-11-28 01:43:14 -0600

    Allow overlays over enbedded Youtube videos

--- src/ATPCms/View/Filter/Youtube.php
+++ src/ATPCms/View/Filter/Youtube.php
@@ -6,6 +6,6 @@ class Youtube extends \ATPCore\View\Filter\AbstractBlockFilter
 {
 	protected function _replace($videoId)
 	{
-		return "<iframe class=\"youtube-player\" type=\"text/html\" src=\"http://www.youtube.com/embed/{$videoId}\" allowfullscreen frameborder=\"0\"></iframe>";
+		return "<iframe class=\"youtube-player\" type=\"text/html\" src=\"http://www.youtube.com/embed/{$videoId}?wmode=transparent\" allowfullscreen frameborder=\"0\"></iframe>";
 	}
 }
